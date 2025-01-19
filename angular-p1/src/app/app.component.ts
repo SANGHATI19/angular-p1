5c2d139c5ef6e93e9bbf2feeaf36e6959f234d6a
SANGHATI19 <nag.sanghati@gmail.com>
2025-01-19 18:23:39 +0530

    datepicker commit 1

--- angular-p1/src/app/app.component.ts
+++ angular-p1/src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatFormFieldModule,MatInputModule,MatDatepickerModule,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-p1';
+  date = new FormControl(new Date());
+ // serializedDate = new FormControl((new Date()).toISOString());
+  serializedDate = new FormControl((new Date().toISOString()));
+
+
 }
+function formatISODate(date: Date): string {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+//const today = new Date();
+//console.log(formatISODate(today));
+
+
